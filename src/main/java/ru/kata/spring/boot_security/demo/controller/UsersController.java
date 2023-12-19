ddbc7a00056dc13eb8ff7c6d9847609a22a2a0d3
Vyacheslav <web@staragro.ru>
2023-12-19 13:12:07 +0300

    done

--- src/main/java/ru/kata/spring/boot_security/demo/controller/UsersController.java
+++ src/main/java/ru/kata/spring/boot_security/demo/controller/UsersController.java
@@ -3,7 +3,6 @@ package ru.kata.spring.boot_security.demo.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import ru.kata.spring.boot_security.demo.models.User;
 import ru.kata.spring.boot_security.demo.service.UserService;
 
@@ -11,7 +10,6 @@ import java.security.Principal;
 
 
 @Controller
-@RequestMapping("/user")
 public class UsersController {
     private final UserService userService;
 
